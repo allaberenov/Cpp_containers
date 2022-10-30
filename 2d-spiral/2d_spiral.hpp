d9563553b5536b54bd5105aaed2db4f75bb36876
kerim <allaberenov.kerim.21@gmail.com>
2022-10-31 00:00:02 +0300

    3rd

--- 2d-spiral/2d_spiral.hpp
+++ 2d-spiral/2d_spiral.hpp
@@ -1,20 +1,20 @@
-int** MakeSpiral(int n) {
-  int** a = new int*[n];
+int **MakeSpiral(int n) {
+  int **a = new int *[n];
   for (int i = 0; i < n; i++) {
     a[i] = new int[n];
   }
   int counter = 0;
-  for(int i = 0; i < n; i++){
-    for(int j = i; j < n - i - 1; j++){
+  for (int i = 0; i < n; i++) {
+    for (int j = i; j < n - i - 1; j++) {
       a[i][j] = ++counter;
     }
-    for(int j = i; j < n - i - 1; j++){
+    for (int j = i; j < n - i - 1; j++) {
       a[j][n - i - 1] = ++counter;
     }
-    for(int j = n - i - 1; j >= i; j--){
+    for (int j = n - i - 1; j >= i; j--) {
       a[n - i - 1][j] = ++counter;
     }
-    for(int j = n - i - 2; j > i; j--){
+    for (int j = n - i - 2; j > i; j--) {
       a[j][i] = ++counter;
     }
   }
