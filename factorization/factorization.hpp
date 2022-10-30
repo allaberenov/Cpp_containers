686ae34e5f41347ad5b2f0220dd992a8d1bed9a0
kerim <allaberenov.kerim.21@gmail.com>
2022-10-30 18:34:06 +0300

    factorization

--- factorization/factorization.hpp
+++ factorization/factorization.hpp
@@ -1,7 +1,16 @@
 #pragma once
 #include <vector>
 
-std::vector<int> Factorize(int /*n*/) {
-    /* Not Implemented */
-    return {};
+std::vector<int> Factorize(int n) {
+  std::vector<int> a;
+  int i = 2;
+  while (i < n) {
+    if (!(n % i)) {
+      a.push_back(i);
+      n = n / i;
+      continue ;
+    }
+    i++;
+  }
+  return a;
 }
