6a1c68046939d1e0fdd696b56a4cf59cd0c96953
kerim <allaberenov.kerim.21@gmail.com>
2022-10-30 18:37:06 +0300

    factorization

--- factorization/factorization.hpp
+++ factorization/factorization.hpp
@@ -5,12 +5,15 @@ std::vector<int> Factorize(int n) {
   std::vector<int> a;
   int i = 2;
   while (i < n) {
-    if (!(n % i)) {
+    if ((n % i) != 0) {
       a.push_back(i);
       n = n / i;
-      continue ;
+      continue;
     }
     i++;
+  }
+  if (a != 0 || a ! = 1) {
+    a.push_back(n)
   }
   return a;
 }
