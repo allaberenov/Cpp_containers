6124c8d3656705d3dc4617db755eb814562c094f
allaberenov <99034932+allaberenov@users.noreply.github.com>
2022-11-02 03:35:13 +0300

    first attempt

--- bin_search/bin_search.hpp
+++ bin_search/bin_search.hpp
@@ -1,6 +1,20 @@
 #pragma once
 
-int* LowerBound(int* /* first */, int* /* last */, int /* value */) {
-    /* Not Implemented */
-    return nullptr;
+int* LowerBound(int* first, int* last, int val) {
+    last--;
+    while (last - first > 1) {
+        int middle = (last - first) / 2;
+        if (*(first + middle) > val) {
+            last = first + middle;
+            continue;
+        }
+        if (*(first + middle) < val) {
+            first = first + middle;
+            continue;
+        }
+        if (*(first + middle) >= val) {
+            return first + middle;
+        }
+    }
+    return last;
 }
