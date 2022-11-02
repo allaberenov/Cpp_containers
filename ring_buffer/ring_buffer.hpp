46dbddf79707a0eb9e82b9a4ae9f5839c2892108
kerim <allaberenov.kerim.21@gmail.com>
2022-11-02 23:37:27 +0300

    first attempt

--- ring_buffer/ring_buffer.hpp
+++ ring_buffer/ring_buffer.hpp
@@ -2,24 +2,64 @@
 
 #include <cstddef>
 
+using namespace std;
+
 class RingBuffer {
- public:
+public:
+  struct Node {
+    int val;
+    Node *next;
+    explicit Node(int i) {
+      val = i;
+      next = nullptr;
+    }
+  };
+
+  size_t curr_size;
+  size_t size;
+  Node *first;
+  Node *end;
+
   explicit RingBuffer(size_t capacity) {
+    size = capacity;
+    curr_size = 0;
+    first = nullptr;
+    end = nullptr;
   }
 
   size_t Size() const {
-    return 0;
+    return size;
   }
 
   bool Empty() const {
-    return false;
+    return curr_size == 0;
   }
 
   bool TryPush(int element) {
+    if (curr_size == size) {
+      return false;
+    }
+    if (first == nullptr) {
+      first = new Node(element);
+      end = first;
+      curr_size++;
+    } else {
+      end->next = new Node(element);
+      end = end->next;
+      curr_size++;
+    }
     return true;
   }
 
-  bool TryPop(int* element) {
+  bool TryPop(int *element) {
+    if (Empty()) {
+      return false;
+    }
+    Node *temp = first;
+    first = first->next;
+    *element = temp->val;
+    delete temp;
+    this->curr_size--;
     return true;
   }
 };
