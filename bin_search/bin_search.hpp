4c13c7e0a1891d8c1c946a0ef705fefd80ea47a0
allaberenov <99034932+allaberenov@users.noreply.github.com>
2022-11-02 04:39:36 +0300

    fifth attempt

--- bin_search/bin_search.hpp
+++ bin_search/bin_search.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-int *LowerBound(int *first, int *last, int val) {
+int* LowerBound(int* first, int* last, int val) {
   last--;
   while (first < last) {
     int middle = (last - first) / 2;
